e085b0108cc29cc3bb6f11baaa0a3210da38c4af
pidge1000 <pidge.1000@gmail.com>
2018-11-29 14:04:25 +0530

    WIP

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'dist/ecoaching-frontend/index.html'))
 })
 
+
 const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, () => {
    console.log(`Server is running on Port ${PORT}`);
